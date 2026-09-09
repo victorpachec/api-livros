72a98f8b8633bb6ba93b7f8fa962fba2beafd454
victorpachec <victorppp05@gmail.com>
2026-09-09 15:02:39 -0300

    prepara ambiente para a parte 2`

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -1,5 +1,34 @@
-CREATE DATABASE biblioteca_db
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
 
-  SHOW DATABASES;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `Criar um livro` int(11) NOT NULL,
+  `Listar livros` int(11) NOT NULL,
+  `Consultar um livro específico` int(11) NOT NULL,
+  `Atualizar um livro` int(11) NOT NULL,
+  `Excluir um livro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
